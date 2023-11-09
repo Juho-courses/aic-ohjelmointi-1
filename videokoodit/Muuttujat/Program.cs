5a0756c8814eba7113bf2bc370b4965f9492d30d
Juho S <juho.salli@gmail.com>
2023-11-09 19:54:25 +0200

    lisää muuttujavideokoodit

---
+++ videokoodit/Muuttujat/Program.cs
@@ -0,0 +1,21 @@
+﻿int age;
+
+age = 30 + 5;
+
+Console.WriteLine(age);
+
+string name = "Juho";
+Console.WriteLine(name + " on nimi!");
+
+float pi1 = 3.14f;
+Console.WriteLine(pi1);
+
+double pi2 = 3.14;
+Console.WriteLine(pi2);
+
+/*
+määritellään isrunning muuttuja
+*/
+bool isRunning = false;
+
+// age = isRunning;

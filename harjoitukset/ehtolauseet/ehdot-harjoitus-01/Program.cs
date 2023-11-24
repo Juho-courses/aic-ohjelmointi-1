ef9e5c772f47ac76f878c4e1bd6eb1d130f61c97
Juho S <juho.salli@gmail.com>
2023-11-24 10:29:27 +0200

    ehtolauseharkkojen esimerkkiratkaisut ja toistolauseiden videokoodit

---
+++ harjoitukset/ehtolauseet/ehdot-harjoitus-01/Program.cs
@@ -0,0 +1,11 @@
+﻿int age = 37;
+string name = "Jaska";
+
+if (name == "Juho" && age == 37)
+{
+    Console.WriteLine("moi " + name + "!");
+}
+else
+{
+    Console.WriteLine("henkilö ei tunnistettu");
+}
---
+++ videokoodit/Toistolauseet/Program.cs
@@ -0,0 +1,55 @@
+﻿Random rand = new Random();
+for (int i = 0; i < 5; i++)
+{
+    Console.Write(rand.Next(10) + " ");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+string lause = "moikku moi!";
+foreach (var c in lause)
+{
+    Console.Write(c + ", ");
+}
+/*
+
+
+/*
+int i = 0;
+while (i < 4)
+{
+    if (i == 2)
+    {
+        i++;
+        break;
+    }
+    Console.WriteLine(i);
+    i++;
+}
+*/
+
+
+
+// i++ == i = i + 1
+/*
+string sana = "moikku";
+for (int i = 0; i < sana.Length; i++)
+{
+    Console.WriteLine(sana[i]);
+}
+*/
+// suoritus
+// Console.WriteLine(0);
+// Console.WriteLine(1);
+// Console.WriteLine(2);
+// Console.WriteLine(3);
